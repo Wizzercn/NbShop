c464b66de02afc33ce9fd09035e587e0e196f783
Wizzer <wizzer@qq.com>
2017-03-31 21:28:07 +0800

    upgrade: v4.x

--- src/main/java/cn/wizzer/common/annotation/SPlugin.java
+++
@@ -1,8 +0,0 @@
-package cn.wizzer.common.annotation;
-
-/**
- *
- * Created by Wizzer.cn on 2015/7/8.
- */
-public @interface SPlugin {
-}
--- src/main/java/cn/wizzer/common/services/redis/RedisKey.java
+++
@@ -1,9 +0,0 @@
-package cn.wizzer.common.services.redis;
-
-/**
- * Created by wizzer on 2016/11/28.
- */
-public interface RedisKey {
-    String RDKEY_SHOP_GOODS = "shop:goods:";
-    String RDKEY_SHOP_PRODUCT = "shop:product:";
-}
