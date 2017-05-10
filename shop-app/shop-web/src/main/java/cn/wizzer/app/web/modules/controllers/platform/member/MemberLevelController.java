7d1f5a1d34e0111f16bc9617eaad4e2e107c3e29
Wizzer <wizzer@qq.com>
2017-05-10 15:36:12 +0800

    add: 会员类型、会员等级管理

---
+++ shop-app/shop-web/src/main/java/cn/wizzer/app/web/modules/controllers/platform/member/MemberLevelController.java
@@ -0,0 +1,114 @@
+package cn.wizzer.app.web.modules.controllers.platform.member;
+
+import cn.wizzer.framework.base.Result;
+import cn.wizzer.app.web.commons.slog.annotation.SLog;
+import cn.wizzer.framework.page.datatable.DataTableColumn;
+import cn.wizzer.framework.page.datatable.DataTableOrder;
+import cn.wizzer.framework.util.StringUtil;
+import cn.wizzer.app.member.modules.models.Member_level;
+import cn.wizzer.app.member.modules.services.MemberLevelService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.nutz.dao.Cnd;
+import org.nutz.lang.Strings;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.mvc.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@IocBean
+@At("/platform/member/level")
+public class MemberLevelController{
+    @Inject
+	private MemberLevelService memberLevelService;
+
+    @At("")
+	@Ok("beetl:/platform/member/level/index.html")
+    @RequiresPermissions("shop.member.config.level")
+	public void index() {
+
+	}
+
+	@At("/data")
+    @Ok("json")
+    @RequiresPermissions("shop.member.config.level")
+    public Object data(@Param("length") int length, @Param("start") int start, @Param("draw") int draw, @Param("::order") List<DataTableOrder> order, @Param("::columns") List<DataTableColumn> columns) {
+		Cnd cnd = Cnd.NEW();
+    	return memberLevelService.data(length, start, draw, order, columns, cnd, null);
+    }
+
+    @At("/add")
+    @Ok("beetl:/platform/member/level/add.html")
+    @RequiresPermissions("shop.member.config.level")
+    public void add() {
+
+    }
+
+    @At("/addDo")
+    @Ok("json")
+    @RequiresPermissions("shop.member.config.level.add")
+    @SLog(tag = "Member_level", msg = "${args[0].id}")
+    public Object addDo(@Param("..")Member_level memberLevel, HttpServletRequest req) {
+		try {
+			memberLevelService.insert(memberLevel);
+			return Result.success("system.success");
+		} catch (Exception e) {
+			return Result.error("system.error");
+		}
+    }
+
+    @At("/edit/?")
+    @Ok("beetl:/platform/member/level/edit.html")
+    @RequiresPermissions("shop.member.config.level")
+    public void edit(String id,HttpServletRequest req) {
+		req.setAttribute("obj", memberLevelService.fetch(id));
+    }
+
+    @At("/editDo")
+    @Ok("json")
+    @RequiresPermissions("shop.member.config.level.edit")
+    @SLog(tag = "Member_level", msg = "${args[0].id}")
+    public Object editDo(@Param("..")Member_level memberLevel, HttpServletRequest req) {
+		try {
+            memberLevel.setOpBy(StringUtil.getUid());
+			memberLevel.setOpAt((int) (System.currentTimeMillis() / 1000));
+			memberLevelService.updateIgnoreNull(memberLevel);
+			return Result.success("system.success");
+		} catch (Exception e) {
+			return Result.error("system.error");
+		}
+    }
+
+    @At({"/delete/?", "/delete"})
+    @Ok("json")
+    @RequiresPermissions("shop.member.config.level.delete")
+    @SLog(tag = "Member_level", msg = "${req.getAttribute('id')}")
+    public Object delete(String id, @Param("ids")  String[] ids, HttpServletRequest req) {
+		try {
+			if(ids!=null&&ids.length>0){
+				memberLevelService.delete(ids);
+    			req.setAttribute("id", org.apache.shiro.util.StringUtils.toString(ids));
+			}else{
+				memberLevelService.delete(id);
+    			req.setAttribute("id", id);
+			}
+            return Result.success("system.success");
+        } catch (Exception e) {
+            return Result.error("system.error");
+        }
+    }
+
+    @At("/detail/?")
+    @Ok("beetl:/platform/member/level/detail.html")
+    @RequiresPermissions("shop.member.config.level")
+	public void detail(String id, HttpServletRequest req) {
+		if (!Strings.isBlank(id)) {
+            req.setAttribute("obj", memberLevelService.fetch(id));
+		}else{
+            req.setAttribute("obj", null);
+        }
+    }
+
+}
