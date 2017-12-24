23344667bee476929ac8b6743bdc83d76e1b9889
wizzercn <wizzer@qq.com>
2017-12-24 17:54:28 +0800

    add: token key存在redis

--- shop-app/shop-web/src/main/java/cn/wizzer/app/sys/modules/services/impl/SysApiServiceImpl.java
+++ shop-app/shop-web/src/main/java/cn/wizzer/app/sys/modules/services/impl/SysApiServiceImpl.java
@@ -2,14 +2,15 @@ package cn.wizzer.app.sys.modules.services.impl;
 
 import cn.wizzer.app.sys.modules.models.Sys_api;
 import cn.wizzer.app.sys.modules.services.SysApiService;
-import cn.wizzer.app.web.commons.base.Globals;
 import cn.wizzer.framework.base.service.BaseServiceImpl;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.crypto.MacProvider;
 import org.nutz.dao.Dao;
+import org.nutz.ioc.aop.Aop;
 import org.nutz.ioc.loader.annotation.IocBean;
-import org.nutz.lang.Files;
+
+import static org.nutz.integration.jedis.RedisInterceptor.jedis;
 
 import java.io.*;
 import java.security.Key;
@@ -24,28 +25,47 @@ public class SysApiServiceImpl extends BaseServiceImpl<Sys_api> implements SysAp
         super(dao);
     }
 
+    @Aop("redis")
     private Key getKey(String appId) throws IOException, ClassNotFoundException {
         Key key;
-        File f = new File(Globals.AppRoot + "/WEB-INF/apikey/" + appId + ".key");
-        if (Files.isFile(f)) {
-            ObjectInputStream keyIn = new ObjectInputStream(new FileInputStream(f));
+        byte[] obj = jedis().get(appId.getBytes());
+        if (obj != null) {
+            ObjectInputStream keyIn = new ObjectInputStream(new ByteArrayInputStream(obj));
             key = (Key) keyIn.readObject();
             keyIn.close();
         } else {
             key = MacProvider.generateKey();
-            Files.createNewFile(f);
-            ObjectOutputStream keyOut = new ObjectOutputStream(new FileOutputStream(f));
-            keyOut.writeObject(key);
-            keyOut.close();
+            ByteArrayOutputStream bao = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bao);
+            oos.writeObject(key);
+            obj = bao.toByteArray();
+            jedis().set(appId.getBytes(), obj);
         }
         return key;
     }
 
+//    private Key getKey(String appId) throws IOException, ClassNotFoundException {
+//        Key key;
+//        File f = new File(Globals.AppRoot + "/WEB-INF/apikey/" + appId + ".key");
+//        if (Files.isFile(f)) {
+//            ObjectInputStream keyIn = new ObjectInputStream(new FileInputStream(f));
+//            key = (Key) keyIn.readObject();
+//            keyIn.close();
+//        } else {
+//            key = MacProvider.generateKey();
+//            Files.createNewFile(f);
+//            ObjectOutputStream keyOut = new ObjectOutputStream(new FileOutputStream(f));
+//            keyOut.writeObject(key);
+//            keyOut.close();
+//        }
+//        return key;
+//    }
+
     /**
      * 生成token
      *
-     * @param date 失效时间
-     * @param appId  appId
+     * @param date  失效时间
+     * @param appId appId
      * @return
      */
     public String generateToken(Date date, String appId) throws IOException, ClassNotFoundException {
